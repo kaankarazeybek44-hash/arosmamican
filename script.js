2674de82ceb2842ab94083f8f7d2938f688c4990
kaankarazeybek44-hash <kaankarazeybek44@gmail.com>
2026-08-11 00:47:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Tek bir arkadaş için özel bilgiler
+const arkadas = {
+    ad: "Aroş mami", 
+    unvan: "Efsane Unvanı / Açıklaması (Örn: Goat of the gourmets)", 
+    foto: "aroş.png" // Klasöre attığın fotoğrafın tam adı
+};
+
+// Sayfadaki HTML yapısını tek bir kart olarak dolduruyoruz
+const container = document.getElementById("galleryContainer");
+
+const card = document.createElement("div");
+card.classList.add("card");
+
+card.innerHTML = `
+    <img src="${arkadas.foto}" alt="${arkadas.ad}">
+    <div class="card-content">
+        <div class="name">${arkadas.ad}</div>
+        <div class="title-text">${arkadas.unvan}</div>
+    </div>
+`;
+
+container.appendChild(card);
